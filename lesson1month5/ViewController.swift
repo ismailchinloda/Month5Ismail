f8a2e2766686e8b300cd71d9344fbbd133444e77
Ismail Chinloda <ismailchinloda@gmail.com>
2023-03-09 20:19:18 +0600

    Initial Commit

---
+++ lesson1month5/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  lesson1month5
+//
+//  Created by Ismail Chinloda on 9/3/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
